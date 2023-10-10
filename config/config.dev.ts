308fd3c192039dc07f063e743810e8783d011283
Qui <qui.ngo@finviet.com.vn>
2023-10-10 18:18:28 +0700

    update config

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -2,8 +2,8 @@ import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
   define: {
-    SERVER_API: 'https://apigootax-ngongocqui1995.cloud.okteto.net',
-    // SERVER_API: 'http://localhost:3333',
+    // SERVER_API: 'https://apigootax-ngongocqui1995.cloud.okteto.net',
+    SERVER_API: 'http://localhost:3333',
     SOCKET_URL: 'https://apigootax-ngongocqui1995.cloud.okteto.net',
     HOST_NAME: 'http://localhost:8000',
     UPLOAD_FILE_SIZE: 1048576 * 100,
