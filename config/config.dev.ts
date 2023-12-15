77ac95867e78fc7111dc0bbfafa697c664315b38
vokhanh1207@gmail.com <vokhanh1207@gmail.com>
2023-12-16 01:22:04 +0700

    update gg map key

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     // SERVER_API: 'https://apigootax-ngongocqui1995.cloud.okteto.net',
     SERVER_API: 'http://localhost:3333',
     SOCKET_URL: 'https://apigootax-ngongocqui1995.cloud.okteto.net',
-    GOOGLE_MAPS_APIKEY: 'AIzaSyDuG_Yw3NrzoN79fIWHnE10-9zSbcNvJK8',
+    GOOGLE_MAPS_APIKEY: 'AIzaSyAvx7mvIIqcXLu0RkxRsrPZnbN-n5QHook',
     HOST_NAME: 'http://localhost:8000',
     UPLOAD_FILE_SIZE: 1048576 * 100,
   },
