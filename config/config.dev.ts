d77e7ac8dd5876d7289ea352ddc050da4de6df18
vokhanh1207@gmail.com <vokhanh1207@gmail.com>
2023-12-17 09:17:51 +0700

    new api key

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     // SERVER_API: 'https://apigootax-ngongocqui1995.cloud.okteto.net',
     SERVER_API: 'http://localhost:3333',
     SOCKET_URL: 'https://apigootax-ngongocqui1995.cloud.okteto.net',
-    GOOGLE_MAPS_APIKEY: 'AIzaSyAvx7mvIIqcXLu0RkxRsrPZnbN-n5QHook',
+    GOOGLE_MAPS_APIKEY: 'AIzaSyBSa7gzHFB6uwu8tWwzb9ZSjnicUGylzKo',
     HOST_NAME: 'http://localhost:8000',
     UPLOAD_FILE_SIZE: 1048576 * 100,
   },
